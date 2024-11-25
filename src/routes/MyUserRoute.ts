87476e1e30780f252f096c801d1a6512aee9e7cf
yashje <yash959973@gmail.com>
2024-11-25 11:36:55 +0530

    Initial commit

---
+++ src/routes/MyUserRoute.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { getCurrentUser, createCurrentUser, updateCurrentUser } from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+// /api/my/user
+router.get("/", jwtCheck, jwtParse, getCurrentUser);
+router.post("/", jwtCheck, createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, updateCurrentUser);
+
+export default router;
